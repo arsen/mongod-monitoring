276856f21f3587ed9ebe962ecdb6f125104b18f5
Arsen Ghazaryan <arsen.ghazaryan@aofl.com>
2017-11-30 15:38:17 -0800

    add counter logic

--- index.js
+++ index.js
@@ -3,28 +3,42 @@ const sgMail = require('@sendgrid/mail');
 
 // console.log(process.env);
 
-const sendAlert = (txt = '') => {
+const sendAlert = (subject, txt) => {
   sgMail.setApiKey(process.env.SENDGRID_KEY);
   const msg = {
     to: process.env.EMAIL,
     from: 'noreply@localhost',
-    subject: 'Mongodb is down',
-    text: new Date().toString() + ' : mongodb is down restarted the process\n-----------------\n' + txt
+    subject: subject,
+    text: txt
   };
   sgMail.send(msg);
 };
 
 const { spawnSync } = require('child_process');
 
+let counter = 0;
+
 setInterval(() => {
   const mongod = spawnSync('ps', ['-A']);
   let time = new Date();
   if (mongod.stdout.toString().indexOf('mongod') === -1) {
-    const mongodRestart = spawnSync('service', ['mongod', 'start']);
-    sendAlert(mongodRestart.stdout.toString());
-    console.log(time.toString() + ' : mongodb is down restarted the process');
+    if (counter <= 5) {
+      counter++;
+      const mongodRestart = spawnSync('service', ['mongod', 'start']);
+      let emailTxt = 
+      `${time.toString()} : mongodb was down restarted the process #${counter} time
+      -----------------
+      ${mongodRestart.stdout.toString()}`;
+
+      sendAlert('Mongodb restarted!', emailTxt);
+      console.log(time.toString() + ' : mongodb was down restarted the process');
+    }
+    else {
+      sendAlert('MONGO DOWN: CAN NOT RESTART!!!', 'MONGO IS DOWN, CAN NOT RESTART');
+    }
   } else {
-    console.log(time.toString() + ' : all good, mongo is running');
+    counter = 0;
+    console.log(time.toString() + ' : all good, mongodb is running :)');
   }
 }, 15000);
 
