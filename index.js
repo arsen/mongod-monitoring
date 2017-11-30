a88d048ee8c793cc962f3d880b1d7eb565c2d214
Arsen Ghazaryan <arsen.ghazaryan@aofl.com>
2017-11-30 15:24:41 -0800

    fix on how to check for mongo proceess and add a timestamp

--- index.js
+++ index.js
@@ -9,18 +9,22 @@ const sendAlert = (txt = '') => {
     to: process.env.EMAIL,
     from: 'noreply@localhost',
     subject: 'Mongodb is down',
-    text: 'mongodb is down restarted the process\n' + txt
+    text: new Date().toString() + ' : mongodb is down restarted the process\n-----------------\n' + txt
   };
   sgMail.send(msg);
 };
 
 const { spawnSync } = require('child_process');
-const mongod = spawnSync('ps', ['A']);
 
-if (mongod.stdout.toString().indexOf('mongod') === -1) {
-  const mongodRestart = spawnSync('service', ['mongod', 'start']);
-  sendAlert(mongodRestart.stdout.toString());
-  console.log('mongodb is down restarted the process');
-} else {
-  console.log('all good, mongo is running');
-}
+setInterval(() => {
+  const mongod = spawnSync('ps', ['-A']);
+  let time = new Date();
+  if (mongod.stdout.toString().indexOf('mongod') === -1) {
+    const mongodRestart = spawnSync('service', ['mongod', 'start']);
+    sendAlert(mongodRestart.stdout.toString());
+    console.log(time.toString() + ' : mongodb is down restarted the process');
+  } else {
+    console.log(time.toString() + ' : all good, mongo is running');
+  }
+}, 15000);
+
